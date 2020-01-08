8deff789efcb4bf748bc152a434238f41cfcf330
An Phan <antphan98@gmail.com>
2020-01-08 13:43:08 -0600

    added connection to server.js

--- db/schema.sql
+++ db/schema.sql
@@ -5,7 +5,7 @@ USE burgers_db;
 CREATE TABLE burgers (
 item_id INTEGER NOT NULL AUTO_INCREMENT,
 burger_name VARCHAR (30),
-devoured BOOLEAN DEFAULT TRUE,
+devoured BOOLEAN DEFAULT FALSE,
 PRIMARY KEY (item_id)
 
 );
